f72af7c18cfe6874d46c59114d80882484789b91
calvinhamus <calvinhamus@u.boisestate.edu>
2014-09-22 05:12:10 -0600

    minir html stuff

---
+++ src/main/java/course/cloud/computing/SocialNetwork/HTMLService.java
@@ -0,0 +1,20 @@
+package course.cloud.computing.SocialNetwork;
+
+	import javax.ws.rs.GET;
+	import javax.ws.rs.Path;
+	import javax.ws.rs.Produces;
+	import javax.ws.rs.core.MediaType;
+
+	@Path("/button")
+	public class HTMLService {
+		
+		@GET
+		@Produces(MediaType.TEXT_HTML)
+		@Path("login")
+		public String getDivContents(){
+			return "<button type='button' name='tracker-button' onclick='reportClick()'> Like! </button>";
+		}
+
+	}
+
+
